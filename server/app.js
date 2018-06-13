b2aaaf3901c155f7bfa4dd041eb53abf5628d9f2
51ding <houhanbin121456@163.com>
2018-06-13 23:52:05 +0800

    接入微信，自动回复，生成菜单

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session=require("koa-session");
-
+var WeChat=require("koa-easywechat");
 require("./models");
 
 const index = require('./routes/index')
@@ -21,13 +21,33 @@ app.keys = config.keys;
 
 // error handler
 onerror(app)
+app.use(logger())
+app.use(WeChat({
+  appID:"wx4277ef79398014a1",
+  appsecret:"2e1dbf5ca9b1634364370b63fe67e3e8",
+  token:"caowei",
+  isSafeModel:false
+},async function (next){
+	 this.reply = {
+        type: "text",
+        content: "感谢关注【甘肃城乡服务平台】\n 现在所有功能仍在紧张开发中，敬请期待！"
+      }
+ }
+));
 
+
+
+
+
+app.listen(833,()=>{
+  console.log("server is running")
+});
 // middlewares
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
-app.use(logger())
+
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
@@ -36,7 +56,7 @@ app.use(views(__dirname + '/views', {
 
 app.use(session(config.session,app))
 
-app.use(auth);
+/*app.use(auth);*/
 
 
 // logger
@@ -56,6 +76,6 @@ app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 });
 
-app.listen(3001,()=>{
+app.listen(3000,()=>{
 	console.log("server is runining...");
 })
---
+++ server/wechat/menu/index.js
@@ -0,0 +1,53 @@
+/*
+ 1、自定义菜单最多包括3个一级菜单，每个一级菜单最多包含5个二级菜单。
+ 2、一级菜单最多4个汉字，二级菜单最多7个汉字，多出来的部分将会以“...”代替。
+ 3、创建自定义菜单后，菜单的刷新策略是，在用户进入公众号会话页或公众号profile页时，如果发现上一次拉取菜单的请求在5分钟以前，就会拉取一下菜单，
+      如果菜单有更新，就会刷新客户端的菜单。测试时可以尝试取消关注公众账号后再次关注，则可以看到创建后的效果。*/
+
+/*菜单的按钮类型*/
+const BUTTON_TYPE={
+	CLICK:"click",
+	VIEW:"view",
+	SCANCODE_PUSH:"scancode_push",
+	SCANCODE_WAITMSG:"scancode_waitmsg",
+	PIC_SYSPHOTO:"pic_sysphoto",
+	PIC_PHOTO_OR_ALBUM:"pic_photo_or_album",
+	PIC_WEIXIN:"pic_weixin",
+	LOCATION_SELECT:"location_select",
+	MEDIA_ID:"media_id",
+	VIEW_LIMITED:"view_limited"
+}
+
+
+
+/*一级菜单*/
+function Button(option){
+	if(!option) throw new Error("无法初始化按钮");
+    this["sub_button"]=[];
+    this.type=option.type;
+    this.name=option.name;
+    this.url=option.url;
+}
+
+Button.prototype.add=function(subButton){
+	if(!(subButton instanceof Button)) return throw new Error("无效的按钮类型");
+	if(this["sub_button"].length==5) return;
+	this["sub_button"].push(subButton);
+}
+
+
+/*菜单类*/
+function Menu(){
+	this.button=[];
+}
+
+
+
+Menu.prototype.add=function(button){
+	if()
+}
+
+
+
+
+
